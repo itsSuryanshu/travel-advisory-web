3b0713af4f8e4d11fc2405eee3899354069c1f7c
Suryanshu <1singhsuryanshu@gmail.com>
2025-07-29 19:14:55 -0400

    Fixes to nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Suppress specific webpack warnings from plotly.js dependencies
+    config.ignoreWarnings = [
+      /Critical dependency: the request of a dependency is an expression/,
+      /node_modules\/glslify-deps\/sync\.js/,
+      /node_modules\/glslify\/transform\.js/,
+    ];
+    
+    return config;
+  },
 };
 
 export default nextConfig;
